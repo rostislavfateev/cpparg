9a2a917321263dc327f8dff2e92abe0b1b5a6394
rostyarostya <rostis.love.fateev@gmail.com>
2026-02-02 20:12:41 +0100

    converter improvement draft

--- include/converter.hpp
+++ include/converter.hpp
@@ -47,10 +47,12 @@ namespace argparse
     {
         namespace traits
         {
+            /// @brief Always false type trait for static_asserts in templates.
             template <class...>
             constexpr std::false_type always_false {};
 
-            // Type trait to allow CustomType(const std::string&) user-defined types
+
+            /// @brief Type trait to allow CustomType(const std::string&) user-defined types.
             template<typename T, typename = void>
             struct is_string_constructible : std::false_type
             {};
@@ -60,6 +62,7 @@ namespace argparse
             {};
 
 
+            /// @brief Type trait to check if T is incrementable (supports ++T and T++. 
             template<typename T, typename = void>
             struct is_incrementable : std::false_type
             {};
@@ -68,6 +71,25 @@ namespace argparse
             struct is_incrementable<T, std::void_t<decltype(++std::declval<T&>(),
                                                             std::declval<T&>()++)>> : std::true_type
             {};
+
+
+            /// @brief Check if T is a standard container (vector, list, set, etc.)
+            template<typename T, typename = void>
+            struct is_container : std::false_type
+            {};
+
+            template<typename T>
+            struct is_container<T, std::void_t<typename T::value_type,
+                                                typename T::iterator,
+                                                decltype(std::declval<T>().begin()),
+                                                decltype(std::declval<T>().end())>> : std::true_type
+            {};
+
+            /// @brief Extract element type from container
+            template<typename T>
+            struct container_element_type {
+                using type = typename T::value_type;
+            };
         } // namespace traits
     } // namespace type
 }
@@ -77,6 +99,35 @@ namespace argparse
 {
     class converter
     {
+    private:
+
+        /// @todo may be more fancy looking (and efficient) 
+        template<typename Container>
+        static Container parse_container(const std::string& value)
+        {
+            using ElementType = typename type::traits::container_element_type<Container>::type;
+            Container result;
+            
+            size_t posS = 0;
+            size_t posE = 0;
+            
+            while ((posE = value.find(' ', posS)) != std::string::npos) {
+                if (posE > posS) {
+                    result.insert(result.end(), 
+                        convert<ElementType>(value.substr(posS, posE - posS)));
+                }
+                posS = posE + 1;
+            }
+            
+            // Handle last element
+            if (posS < value.length()) {
+                result.insert(result.end(), 
+                    convert<ElementType>(value.substr(posS)));
+            }
+            
+            return result;
+        }
+
     public:
 
         template<typename T>
@@ -86,66 +137,40 @@ namespace argparse
             {
                 return T{};
             }
-            if      constexpr (std::is_same_v<T, std::string>)
-            {
+
+            // Handle containers (vector, list, set, etc.)
+            if      constexpr (type::traits::is_container<T>()) {
+                return parse_container<T>(value);
+            }
+            // Handle scalar types
+            else if constexpr (std::is_same_v<T, std::string>) {
                 return value;
             }
-            else if constexpr (std::is_same_v<T, bool>)
-            {
+            else if constexpr (std::is_same_v<T, bool>) {
                 return value == "1" || value == "true";
             }
-            else if constexpr (std::is_same_v<T, float>)
-            {
+            else if constexpr (std::is_same_v<T, float>) {
                 return std::stof(value);
             }
-            else if constexpr (std::is_same_v<T, double>)
-            {
+            else if constexpr (std::is_same_v<T, double>) {
                 return std::stod(value);
             }
-//            else if constexpr (std::is_same_v<T, char>)
-//            {
-//                return static_cast<char>(value[0]);
-//            }
-//            else if constexpr (std::is_same_v<T, unsigned char>)
-//            {
-//                return static_cast<unsigned char>(value[0]);
-//            }
-            else if constexpr (std::is_same_v<T, int>)
-            {
+            else if constexpr (std::is_same_v<T, int>) {
                 return std::stoi(value);
             }
-            else if constexpr (std::is_same_v<T, unsigned int>)
-            {
+            else if constexpr (std::is_same_v<T, unsigned int>) {
                 return unsigned(std::stoul(value));
             }
-            else if constexpr (std::is_same_v<T, long int>)
-            {
+            else if constexpr (std::is_same_v<T, long int>) {
                 return std::stoll(value);
             }
-            else if constexpr (std::is_same_v<T, unsigned long int>)
-            {
+            else if constexpr (std::is_same_v<T, unsigned long int>) {
                 return std::stoull(value);
             }
-            else if constexpr (type::traits::is_string_constructible<T>())
-            {
+            else if constexpr (type::traits::is_string_constructible<T>()) {
                 return T(value);
             }
-            // TODO: this has to be handled on ArgumentAction level through different constructor -
-            // no complex nested types supported (like vect<vect<str>>)
-//            else if constexpr (std::is_same_v<T, std::vector<int>>)
-//            {
-//                std::vector<int> result;
-//                size_t posS = 0;
-//                size_t posE = 0;
-//                while ((posE = value.find(" ", posS)) != std::string::npos)
-//                {
-//                    result.push_back(std::stoi(value.substr(posS, posE)));
-//                    posS = posE + 1;
-//                }
-//                return result;
-//            }
-            else
-            {
+            else {
                 static_assert(type::traits::always_false<T>, "Unsupported type");
             }
         }
